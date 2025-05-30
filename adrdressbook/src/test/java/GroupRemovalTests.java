e2cc9461859c62229f7bf9e6fc2d88b34b65bb9b
aurum <aurum727@mail.ru>
2025-05-30 23:08:49 +0300

    addressbook_first_commit(GroupCreationTests, GroupRemovalTests)

---
+++ adrdressbook/src/test/java/GroupRemovalTests.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GroupRemovalTests {
+  private static WebDriver driver;
+
+  @BeforeEach
+  public void setUp() {
+    if ( driver==null ) {
+      driver = new ChromeDriver();
+      Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+      driver.get("https://addressbook.sintetika.keenetic.pro/index.php");
+      driver.manage().window().setSize(new Dimension(1476, 894));
+      driver.findElement(By.name("user")).sendKeys("admin");
+      driver.findElement(By.name("pass")).sendKeys("secret");
+      driver.findElement(By.name("pass")).sendKeys(Keys.ENTER);
+    }
+  }
+
+  @Test
+  public void canRemoveGroup() {
+    if (! isElementPresent(By.name("new"))){
+      driver.findElement(By.linkText("groups")).click();
+    }
+    if (! isElementPresent(By.name("selected[]"))) {
+      driver.findElement(By.name("new")).click();
+      driver.findElement(By.name("group_name")).sendKeys("2");
+      driver.findElement(By.name("group_header")).sendKeys("2");
+      driver.findElement(By.name("group_footer")).sendKeys("2");
+      driver.findElement(By.name("submit")).click();
+      driver.findElement(By.linkText("group page")).click();
+    }
+    driver.findElement(By.name("selected[]")).click();
+    driver.findElement(By.name("delete")).click();
+    driver.findElement(By.linkText("group page")).click();
+  }
+
+  private boolean isElementPresent(By locator) {
+    try {
+      driver.findElement(locator);
+      return true;
+    } catch (NoSuchElementException exception) {
+      return false;
+    }
+  }
+
+}
